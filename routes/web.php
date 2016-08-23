77b166c0a74c579e0ef82d787de8a76385dd558e
Gunharth Randolf <hello@gunharth.io>
2016-08-23 18:52:18 +0200

    minors

--- routes/web.php
+++ routes/web.php
@@ -46,17 +46,11 @@ Route::group(['middleware' => 'auth', 'prefix' => LaravelLocalization::setLocale
      * Dev only reset Database
      */
     Route::get('/resetdb', function () {
-        // Artisan::call('migrate:refresh', [
-        //     '--force' => true,
-        //     '--seed' => true,
-        // ]);
-
-        $exitCode = Artisan::call('migrate:refresh', [
-    '--force' => true,
-]);
-
-        echo($exitCode);
-
+        Artisan::call('migrate:refresh', [
+            '--force' => true,
+            '--seed' => true,
+        ]);
+        
         return redirect('/');
     });
 
