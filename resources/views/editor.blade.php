2aa1c23856bf86825ba5fe0227d13c60bdf2b262
Gunharth Randolf <hello@gunharth.io>
2016-08-29 22:44:28 +0200

    Remove test route blade code

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -131,6 +131,7 @@
 			             <b>DEV ONLY: <a href="/admin/resetdb">Reset Database</a></b> <br>
 			             <b><a href="/admin/sitemap">Generate Sitemap</a> </b><br>
 			             spatie google analytics ! <br>
+			             realfavicongenerator package <br>
 			             Settings (Global Title, etc ... Mail Preferences)
 			             Users / Access <br>
 			             Global Code (GA, Google webmaster tools, Bing webmaster tools, keep open to enter scripts like page specific codes) <br>
